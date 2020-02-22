13f2ae57ff6e6e5f7569aafce98746e0991886cc
Brittany Russell <sincerelybr@gmail.com>
2020-02-22 20:59:03 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,10 +7,17 @@ var playlist = {
 // objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName: songTitle` key-value pair to `playlist`:
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign(playlist, { artistName: songTitle })
+  // Remember, because one of this function's arguments
+  // is `playlist`, the `playlist` _inside_ the function
+  // is not the same as the top-level `playlist` outside
+  // the function
+  playlist[artist] = song
+
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName) {
-  return Object.assign ({}, { [artistName]: songTitle })
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+
+  return playlist
 }
