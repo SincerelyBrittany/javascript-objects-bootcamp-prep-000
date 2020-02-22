223d6d02711d678067ebe932e6ada1b77a3c1803
Brittany Russell <sincerelybr@gmail.com>
2020-02-22 21:00:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   // is `playlist`, the `playlist` _inside_ the function
   // is not the same as the top-level `playlist` outside
   // the function
-  playlist[artist] = song
+  playlist[artistName] = songTit
 
   return playlist
 }
