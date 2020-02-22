665db386952e967df4a34d04b912300c2bfc80ee
Brittany Russell <sincerelybr@gmail.com>
2020-02-22 20:24:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,8 @@ var playlist = {
   TreySongz:"Gotta Go!"
 };
 
+// objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName: songTitle` key-value pair to `playlist`:
+
 function updatePlaylist(artistName, songTitle) {
  
   return Object.assign ({}, { [artistName]: songTitle })
